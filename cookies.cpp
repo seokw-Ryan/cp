6c4d0394e5c4d2426e5ffa154470a9cbb1744d44
Seokwoo Chung (Ryan) <seokwoo.chung130@gmail.com>
2026-01-15 10:52:25 -0500

    cookies

---
+++ cookies.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	int count = 0;
+	for (int i=0; i<n; i++) {
+		int temp;
+		cin >> temp;
+		if (temp%2 == 1) count++; 
+		v.push_back(temp);
+	}
+	
+	int ans;
+	if (count == 1) {
+		ans = 1;
+	} else {
+		ans = n - count;
+	}
+	cout << ans;
+	return 0;
+}
