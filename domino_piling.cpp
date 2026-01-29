2bc70e8e31b9d9c7dd44a83816809095fac51471
Ryan Chung <seokwoo.chung130@gmail.com>
2026-01-29 09:01:33 -0500

    domino_piling

---
+++ domino_piling.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+
+    int M, N;
+    cin >> M >> N;
+
+    int total_sq = M * N;
+    int ans = total_sq/2;
+    cout << ans;
+    return 0;
+}
