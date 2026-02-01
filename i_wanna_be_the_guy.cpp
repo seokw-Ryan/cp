ecf9f8646132106d2978c80acbe4a588b56400a6
Seokwoo Chung (Ryan) <seokwoo.chung130@gmail.com>
2026-02-01 16:23:22 -0500

    solve i wanna be the guy problem that i did not solve yesterday using cpp sets.

--- i_wanna_be_the_guy.cpp
+++ i_wanna_be_the_guy.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main() {
 
-	int n, p_x;
+	int n;
 	cin >> n;
+	set<int> s;
+	
+	// Little X
+	int p_x;
 	cin >> p_x;
-	vector<int> level;
-	int curr_l = 1;
 	for (int i=0; i<p_x; i++) {
 		int temp;
 		cin >> temp;
-		if (temp == curr_l) curr_l++;
-	
+		s.insert(temp);
 	}
+
+	//Little Y
 	int p_y;
 	cin >> p_y;
-	for(int i=0; i<p_y; i++) {
+	for (int i=0; i<p_y; i++) {
 		int temp;
 		cin >> temp;
-		if (temp == curr_l) curr_l++;
+		s.insert(temp);
 	}
 
-//	cout << "curr_l: " << curr_l << "\n";
-
-	if (n == curr_l - 1) cout << "I become the guy.";
+	if (s.size() == n) cout << "I become the guy.";
 	else cout << "Oh, my keyboard!";
 
 
