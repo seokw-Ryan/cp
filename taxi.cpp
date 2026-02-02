7c8d749645dda4a65300bfbbb6a6cb888db8c489
Seokwoo Chung (Ryan) <seokwoo.chung130@gmail.com>
2026-02-02 11:40:08 -0500

    technically, i didnt solve this in time but for now i am saying that i was there. The problem was that when you are using ceil(), you need to make sure you force floating poitn division first.

--- taxi.cpp
+++ taxi.cpp
@@ -1,48 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main() {
 
 	int n;
 	cin >> n;
-	vector<int> v;
-	// going to try two pointers
-	// sort then have two pointers
+	int c1=0, c2=0, c3=0, c4=0;
 	for (int i=0; i<n; i++) {
 		int temp;
 		cin >> temp;
-		v.push_back(temp);
+		if (temp == 1) c1++;
+		else if (temp == 2) c2++;
+		else if (temp == 3) c3++;
+		else c4++;
 	}
-	sort(v.begin(), v.end());
 
-	int p1 = 0;
-	int p2 = v.size()-1;
-	int num_c = 0;
-	int temp = 0;
-	while (p1 <= p2) {
-		if (v[p2] == 4) {
-			p2--;
-			num_c++;
-		} else if (v[p1] + v[p2] + temp == 4) {
-			p1++;
-			p2--;
-			num_c++;
-			temp = 0;
-		} else if (v[p1] + v[p2] + temp > 4){
-			p1++;
-			p2--;
-			num_c += 2;
-			temp = v[p1] + v[p2] - 4 ;
-		} else if (v[p1] + v[p2] + temp < 4) {
-			temp += v[p1] + v[p2];
-			p1++;
-			p2--;
-		}
+	//start from 4 then to 1
+	int ans = 0;
+	//add all of 4
+	ans += c4;
+	// add all of 3
+	// subtract num of 3 from num of 1 since all the 1 can fit in with 3.
+	ans += c3;
+	if (c1 > c3) c1 -= c3;
+	else c1 = 0;
+	// add half of 2 since we only need one car for two 2.	
+	ans += c2/2;
+	if (c2%2 == 1) {
+		if (c1 >= 2) c1 -= 2;
+		else if (c1 == 1) c1 -=1;
+		ans++;
 	}
+	// add remaining 1
+	if (c1 > 0) ans += ceil(c1/4.0) ;
+	cout << ans;
 	
-	cout << num_c;
+
+
 
 	return 0;
 }
-
-
