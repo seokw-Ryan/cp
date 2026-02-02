3e164979b2ae317e3cc0f63019ad9163d2855210
Seokwoo Chung (Ryan) <seokwoo.chung130@gmail.com>
2026-02-02 11:52:05 -0500

    solved dragons.cpp. noticed that implementation is still not the strongest when it comes to pair<int, int> in terms of how to implement it. Still need to be comfortable using custom sort even though custom sort was not needed for this problem.

--- dragons.cpp
+++ dragons.cpp
@@ -1,29 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+using ll = long long;
 
 int main() {
 
-    int s, n;
-    cin >> s >> n;
-    int total_p = s;
-    bool check =true;
-    for (int i=0; i<n; i++) {
-        int xi, yi;
-        cin >> xi >> yi;
-        
-        if (total_p > xi) {
-            total_p += yi;
-        } else {
-		check = false;
-		break;
-        } 
-     
-    }
-    if (check == false) cout << "NO"; else cout << "YES";
-    
-    
+	int s, n;
+	cin >> s >> n;
+	vector<pair<int, int>> v;
+	bool can_w = true;
+	for (int i=0; i<n; i++) {
+		int x1, x2;
+		cin >> x1 >> x2;
+		v.push_back({x1, x2});
+	}
+	// problem is here. Is this the right way to sort v?
+	sort(v.begin(), v.end());
+	
+	for (int i=0; i<n; i++) {
+		if (v[i].first < s) {
+			s += v[i].second;
+		} else {
+			can_w = false;
+			break;
+		}
+	}
 
-    return 0;
+	if (can_w) cout << "YES"; else cout << "NO";
+
+
+
+
+
+
+	return 0;
 }
-
