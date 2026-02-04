fcc3aa4ef98b8b7759a675dffb49d5e5cafc20f5
Seokwoo Chung (Ryan) <seokwoo.chung130@gmail.com>
2026-02-03 20:14:03 -0500

    upsolved interesting drink. not really a dp problem but still interesting. it is clear that i still have quite a distance to go regarding the implementation and the usage of cpp functions such as upper bound and lower bound

--- interesting_drink.cpp
+++ interesting_drink.cpp
@@ -6,28 +6,34 @@ int main() {
 
 	int n;
 	cin >> n;
-	vector<ll> v;
+	vector<int> v;
 	for (int i=0; i<n; i++) {
-		ll x; cin >> x;
+		int x; cin >> x;
 		v.push_back(x);
 	}
+	sort(v.begin(), v.end());
 	int q;
 	cin >> q;
-	vector<ll> ans;
 	for (int i=0; i<q; i++) {
-		ll m; cin >> m;
-		// each day
-		ll count = 0;
-		for (int j=0; j<n; j++) {
-			if (v[j] <= m) count ++;
-		}
-		ans.push_back(count);
+		ll num_c; cin >> num_c;
+		/*for (int j=0; j<n; j++) {
+			ll beer_c = (ll)v[j];
+			if (num_c >= beer_c) {
+				//num_c -= beer_c;
+				num_s++;
+			} else {
+				break;
+			}
+		}*/
+		// it is not so good that i kinda remember the solution
+		// so basically search for the position of the greatest number
+		// that is less than num_c
+		// and the implementation here needs definite improvement
+		auto it = upper_bound(v.begin(), v.end(), num_c);
+		int num_s = distance(v.begin(), it);
 
-
-	}
-
-	for (int i=0; i<q; i++) {
-		cout << ans[i] << "\n";
+		//output each loop
+		cout << num_s << "\n";
 	}
 
 
