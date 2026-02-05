700759279d0a58745eb2dd294809a76610c27e58
Seokwoo Chung (Ryan) <seokwoo.chung130@gmail.com>
2026-02-04 23:02:05 -0500

    solved cut_ribbon. fundamental dp question. surprised that i solved it. very interesting

--- cut_ribbon.cpp
+++ cut_ribbon.cpp
@@ -2,26 +2,25 @@
 using namespace std;
 using ll = long long;
 
-int main() {
-
+int main() { 
+	
 	int n, a, b, c;
 	cin >> n >> a >> b >> c;
-	// i guess we store the num of ribbon pieces
-	vector<int> v;
-	v.push_back(a);
-	v.push_back(b);
-	v.push_back(c);
-	sort(v.begin(), v.end());
 
-	vector<int> dp;
-	int ans = 0;
-	int temp = n;
-	for (int i=0; i<3; i++) {
-		temp = temp - v[i];
-		if (temp >= 0) ans++;
+	int neg = INT_MIN;
+	vector<int> dp(n+1, neg);
+	int ans;
+	// think about creating a vector where we are having the length of the
+	// ribbon as dp[i] val
+	// x as dp[0] = 
+	dp[0] = 0;
+	for (int i=0; i<=n; i++) {
+		if (i-a >= 0) dp[i] = max(dp[i-a]+1, dp[i]);
+		if (i-b >= 0) dp[i] = max(dp[i-b]+1, dp[i]);
+		if (i-c >= 0) dp[i] = max(dp[i-c]+1, dp[i]);
 	}
-	cout << ans;
-	
+	cout << dp[n];
+
 
 
 	return 0;
