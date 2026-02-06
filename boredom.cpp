339ec84af70aade38c570d7e6fa7a0662ba88dd3
Seokwoo Chung (Ryan) <seokwoo.chung130@gmail.com>
2026-02-05 22:58:25 -0500

    failed this question again. Apparently, has a long way to go on dp

--- boredom.cpp
+++ boredom.cpp
@@ -6,19 +6,34 @@ int main() {
 
 	int n;
 	cin >> n;
-	vector<int> dp(n,0);
 	vector<int> v;
-	// num of points as dp[x]
-	for (int i=0; i<n; i++) {
+	vector<int> dp(n, 0);
+	for (int i=0; i< ; i++) {
 		int x; cin >> x;
 		v.push_back(x);
 	}
-	for( int i=0; i<n; i++) {
-		dp[v[i]] += v[i];
-	}
-	auto it = max_element(dp.begin(), dp.end());
-	cout << *it;
 	
+	// think about what needs to be stored
+	// index vs. value
+	// i dont think we are removing any elements here because that will take
+	// too long
+	// the value should be max number of points
+	// dp[i] = max(dp[i], 
+	// but what should "i" be?
+	// the question is how should this second for loop be navigatedf
+	// i guess we only look ak-1 part because in the end we wont need to
+	// look at ak+1
+	// which means that we go through each and every element
+	for (int i=0; i<n; i++) {
+		// whats the best way to go and store the element?
+		// i and i-2
+		if (dp[i] = max(d
+
+		
+
+
+		return 0;
+	}
 
 
 	return 0;
