89d1839d7843f09fe5f24366810affc69b8a12dd
Ryan Chung <seokwoo.chung130@gmail.com>
2026-02-19 09:57:53 -0500

    tried t prime

--- t_primes.cpp
+++ t_primes.cpp
@@ -3,31 +3,34 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int n;
+    cin >> n;
+    //pattern is in prime
+    //create the entire vector of long long and then search that vector
+    ll max_val = LLONG_MAX;
+    vector<int> v;
+    for (int i=0; i<n; i++) {
+        // if prime then put in the vector
+        int x; cin >> x;
+        v.push_back(x);
+    }
 
-	int n;
-	cin >> n;
-	for (int i=0; i<n; i++) {
-		// logic to check if it is t prime
-		int x; cin >> x;
-		bool b = false;
-		// Every number have two. So it's important to find a number
-		int half = x/2;
-		for (int j=2; j<=half; j++) {
-			if (j*j > x) break;
-			else if (j*j == x) {
-				b = true;
-				break;
-			}
-		}
-		if (b == true) cout << "YES" << "\n";
-		else cout << "NO" << "\n";
+    max_val = max(v.begin(), v.end());
+    // get prime values
+    vector<int> v_prime;
+    for (int i=1; i<max_val/2; i++) {
+        bool is_prime = true;
+       for (int j=1; j<i; j++) {
+            if (i%j == 0) is_prime = false;
+       }
+       if (is_prime) v_prime.push_back(i);
+    }
 
-
-	}
-
-
-
-	return 0;
-
+    //binary search
+    for (int i=0; i<n; i++) {
+        // 
+        if ((v[i])^(1/2) 
+    
+    return 0;
 
 }
